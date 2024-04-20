57c0c84dc3b09e47feceb8697f8b15d6dd6d0c76
iamjohnhm <jhm90822@gmail.com>
2024-04-20 09:11:21 +0300

    Style some of the signin page's elements and created a css file called App.css to style some of the signin page's elements

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import '../App.css'
 
 function Signin() {
   return (
-    <div>
-        <form>
-            <h2>Login</h2>
-            <div class="group">
-                <input type="text" /><span class="highlight"></span><span class="bar"></span>
-                <label>Name</label>
+    <div className='flex items-center justify-center mt-[200px]'>
+        <form className='w-[380px] pt-10 pr-10 pb-10 pl-5 bg-[#fafafa] border-[1px] border-[#ebebeb] shadow shadow-lg'>
+            <h2 className='text-[#636363] mb-7 flex items-center justify-center text-[28px]'>Login</h2>
+            <div className="relative mb-[45px]">
+                <input className='text-18 pb-[10px] pr-[10px] pb-[10px] pl-[5px] appearance-none block bg-[#fafafa] text-[#636363] w-full border border-0 border-b border-solid border-gray-600 outline-none' type="email" /><span class="highlight"></span><span class="bar"></span>
+                <label className='text-[#999] text-18 font-normal absolute pointer-events-none left-0 top-3 transition-all duration-200 ease'>Email</label>
             </div>
-            <div class="group">
-                <input type="email" /><span class="highlight"></span><span class="bar"></span>
-                <label>Email</label>
+            <div class="relative mb-[45px]">
+                <input className='text-18 pb-[10px] pr-[10px] pb-[10px] pl-[5px] appearance-none block bg-[#fafafa] text-[#636363] w-full border border-0 border-b border-solid border-gray-600 outline-none' type="password" /><span class="highlight"></span><span class="bar"></span>
+                <label className='text-[#999] text-18 font-normal absolute pointer-events-none left-0 top-3 transition-all duration-200 ease'>Password</label>
             </div>
             <button type="button" class="button buttonBlue">Login
                 <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
