869d7f1e6f5aeb5b23ee46eafdd151813b31fa88
iamjohnhm <jhm90822@gmail.com>
2024-04-21 00:21:35 +0300

    Implemented a logic to display the final result out of ten the user has got after finishing doing the questions and hitting the submit button. Also added some styles to how the result will be displayed.

--- src/pages/McqPage.js
+++ src/pages/McqPage.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 function McqPage() {
     const [questions, setQuestions] = useState([]);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [selectedAnswer, setSelectedAnswer] = useState({});
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,23 +22,34 @@ function McqPage() {
     }, []);
 
     const handleAnswerChange = (event) => {
-        setSelectedAnswer(event.target.value);
+        setSelectedAnswer({ ...selectedAnswer, [currentQuestionIndex]: event.target.value });
     };
 
     const handlePrevious = () => {
         setCurrentQuestionIndex((prevIndex) => Math.max(0, prevIndex - 1));
-        setSelectedAnswer(null); // Reset selected answer when navigating
+        //setSelectedAnswer(null); // Reset selected answer when navigating
     };
 
     const handleNext = () => {
         setCurrentQuestionIndex((prevIndex) => Math.min(prevIndex + 1, questions.length - 1));
-        setSelectedAnswer(null); // Reset selected answer when navigating
+        //setSelectedAnswer(null); // Reset selected answer when navigating
     };
 
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevent form submission (we handle it manually)
+        setShowScore(true);
+
+        //Calculate score based on selected answers
+        let calculatedScore = 0;
+        questions.forEach((question, index) => {
+            if (selectedAnswer[index] === question.correct_answer) {
+                calculatedScore++;
+            }
+        });
+        setScore(calculatedScore);
+
         // Logic to handle submitted answers
-        console.log('Selected Answers:', selectedAnswer);
+        //console.log('Selected Answers:', selectedAnswer);
         // You can compare selectedAnswer with correct answer and calculate score here
     };
 
@@ -50,7 +63,12 @@ function McqPage() {
         <div className='flex mt-[5px] items-center justify-center'>
             <div className='w-[1000px] pt-10 pr-10 pb-10 pl-5 bg-[#fafafa] border-[1px] border-[#ebebeb] shadow shadow-lg'>
               <h1 className='flex items-center justify-center mb-10 text-2xl text-[#636363]'>Multiple Choice Game</h1>
-              <form onSubmit={handleSubmit}>
+              {showScore ? (
+                <div className='flex items-center justify-center border bg-[#4a89dc] pt-2 pb-2 w-[200px] ml-[365px]'>
+                    <h2 className='text-[#fff] text-2xl flex items-center justify-center'>Your Score: {score}/{questions.length}</h2>
+                </div>
+              ):(
+                <form onSubmit={handleSubmit}>
                 <div>
                     <p className='mb-3'><span>{currentQuestionIndex + 1}. </span>{currentQuestion.question}</p>
                     <ul>
@@ -62,7 +80,7 @@ function McqPage() {
                                         type="radio"
                                         name="answer"
                                         value={choice}
-                                        checked={selectedAnswer === choice}
+                                        checked={selectedAnswer[currentQuestionIndex] === choice}
                                         onChange={handleAnswerChange}
                                     />
                                     {choice}
@@ -72,10 +90,10 @@ function McqPage() {
                     </ul>
                 </div>
                 <div className='mt-10 mb-20 flex items-center flex-row gap-[350px]'>
-                    <button className='inline-block pt-4 pb-4 pr-20 pl-16 mt-[.3em] mr-0 mb-[1em] ml-0 w-[25%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
+                    <button type='button' className='inline-block pt-4 pb-4 pr-20 pl-16 mt-[.3em] mr-0 mb-[1em] ml-0 w-[25%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
                         Previous
                     </button>
-                    <button className='inline-block pt-4 pb-4 pr-20 pl-16 mt-[.3em] mr-0 mb-[1em] ml-0 w-[25%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' onClick={handleNext} disabled={currentQuestionIndex === questions.length - 1}>
+                    <button type='button' className='inline-block pt-4 pb-4 pr-20 pl-16 mt-[.3em] mr-0 mb-[1em] ml-0 w-[25%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' onClick={handleNext} disabled={currentQuestionIndex === questions.length - 1}>
                         Next
                     </button>
                 </div>
@@ -83,6 +101,7 @@ function McqPage() {
                 <button className='relative inline-block pt-4 pb-4 pr-20 pl-20 mt-[.3em] mr-0 mb-[1em] ml-0 w-[40%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' type="submit">Submit Answer</button>
                 </div>
               </form>
+              )}
             </div>
         </div>
     );
