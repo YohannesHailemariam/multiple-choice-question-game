6e6da88fe46e152e0044a5b24d776b282fd60c6a
iamjohnhm <jhm90822@gmail.com>
2024-04-20 21:29:34 +0300

    styled the McqPage where the game will be played and also added a logic to display the question numbers currently the user is on before the questions

--- src/pages/McqPage.js
+++ src/pages/McqPage.js
@@ -47,16 +47,18 @@ function McqPage() {
     const currentQuestion = questions[currentQuestionIndex];
 
     return (
-        <div>
-            <h1>Multiple Choice Game</h1>
-            <form onSubmit={handleSubmit}>
+        <div className='flex mt-[5px] items-center justify-center'>
+            <div className='w-[1000px] pt-10 pr-10 pb-10 pl-5 bg-[#fafafa] border-[1px] border-[#ebebeb] shadow shadow-lg'>
+              <h1 className='flex items-center justify-center mb-10 text-2xl text-[#636363]'>Multiple Choice Game</h1>
+              <form onSubmit={handleSubmit}>
                 <div>
-                    <p>{currentQuestion.question}</p>
+                    <p className='mb-3'><span>{currentQuestionIndex + 1}. </span>{currentQuestion.question}</p>
                     <ul>
                         {[...currentQuestion.incorrect_answers, currentQuestion.correct_answer].map((choice, index) => (
-                            <li key={index}>
+                            <li className='mb-3' key={index}>
                                 <label>
                                     <input
+                                        className='mr-3'
                                         type="radio"
                                         name="answer"
                                         value={choice}
@@ -69,16 +71,19 @@ function McqPage() {
                         ))}
                     </ul>
                 </div>
-                <div>
-                    <button onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
+                <div className='mt-10 mb-20 flex items-center flex-row gap-[350px]'>
+                    <button className='inline-block pt-4 pb-4 pr-20 pl-16 mt-[.3em] mr-0 mb-[1em] ml-0 w-[25%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
                         Previous
                     </button>
-                    <button onClick={handleNext} disabled={currentQuestionIndex === questions.length - 1}>
+                    <button className='inline-block pt-4 pb-4 pr-20 pl-16 mt-[.3em] mr-0 mb-[1em] ml-0 w-[25%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' onClick={handleNext} disabled={currentQuestionIndex === questions.length - 1}>
                         Next
                     </button>
                 </div>
-                <button type="submit">Submit Answer</button>
-            </form>
+                <div className='flex items-center justify-center'>
+                <button className='relative inline-block pt-4 pb-4 pr-20 pl-20 mt-[.3em] mr-0 mb-[1em] ml-0 w-[40%] align-middle text-[#fff] text-16 leading-[20px] antialiased text-center tracking-normal bg-transparent border-0 border-b-2 border-b-solid cursor-pointer transition-all duration-150 ease focus:outline-none bg-[#4a89dc]' type="submit">Submit Answer</button>
+                </div>
+              </form>
+            </div>
         </div>
     );
 }
