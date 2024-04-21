87005c009e06383e8d831b28a4612cf7b48b61da
iamjohnhm <jhm90822@gmail.com>
2024-04-22 00:09:48 +0300

    Enabled routing between from signin page to the McqPage and from the McqPage back to the login page.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import Signin from "./pages/Signin";
 import McqPage from "./pages/McqPage";
 import { useSelector } from "react-redux";
 import { selectUser } from "./features/userSlice";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
+
+
 function App() {
   const user = useSelector(selectUser);
 
+  
+
   return (
-    <div>
-      {user ? <McqPage /> : <Signin />}
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Signin} />
+          <Route exact path='/mcq' component={McqPage} />
+        </Switch>
+      </div>
+    </Router>
+    
   );
+
+  
+
 }
 
 export default App;
